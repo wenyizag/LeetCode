3f223cea24ca96139c49c10f1cc4cfaae9209116
Wenyi Zhang <wzhang43@ncsu.edu>
2018-02-13 22:20:29 -0500

    Sort
    easy

---
+++ FirstBadVersion.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class FirstBadVersion {
+	public int firstBadVersion(int n) {
+        int low = 1, high = n;     
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            if(isBadVersion(mid)) high = mid;
+            else low = mid+1;
+        }      
+        return low;        
+    }
+}
