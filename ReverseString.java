e81de9025e03410fc9183b25fe638b0ad95afdbd
Wenyi Zhang <wenyizag@outlook.com>
2018-02-16 19:15:56 -0500

    Strings
    Easy

--- ReverseString.java
+++ ReverseString.java
@@ -4,18 +4,22 @@ import java.util.HashSet;
 import java.util.LinkedList;
 
 public class ReverseString {
-	public static int numDecodings(String s) {
-		int n = s.length();
-		if(n==0) return 0;
-		int[] dp = new int[n+1];
-		dp[0] = 1;
-		dp[1] = (s.charAt(0) == '0') ? 0 : 1;
-		for(int i = 2; i <= n; i++) {
-			int n1 = Integer.valueOf(s.substring(i-2, i));
-			int n2 = Integer.valueOf(s.substring(i-1, i));
-			if(n2 >=1 && n2 <= 9) dp[i] += dp[i-1];
-			if(n1 >=10 && n1 <= 26) dp[i] += dp[i-2];
-		}
-		return dp[n];
+	//方法一
+	public static String reverseString(String s) {
+        char[] s2 = s.toCharArray();
+
+        for(int i = s2.length-1, j = 0; i > j ; i--, j++) {
+        		char tmp = s2[i];
+        		s2[i] = s2[j];
+        		s2[j] = tmp;
+        }
+        return new String(s2);
+    }
+	
+	//方法二
+	public static String reverseString2(String s) {
+		StringBuilder s2 = new StringBuilder(s);
+		return s2.reverse().toString();
 	}
+	
 }
