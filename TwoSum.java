6a027cf769f16e18ff0b42842e7403f95a7d1fd7
Wenyi Zhang <wenyizag@outlook.com>
2018-02-20 11:51:19 -0500

    Array
    Easy

--- TwoSum.java
+++ TwoSum.java
@@ -1,23 +1,17 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class TwoSum {
 	public static int[] twoSum(int[] nums, int target) {
-		 int[] ans = new int[2];
-		 HashMap<Integer, Integer> map = new HashMap();
-		 
-		 for(int i = 0; i < nums.length; i++) {
-			 if(map.containsKey(target-nums[i])) {
-				 ans[0] = map.get(target-nums[i]);
-				 ans[1] = i;
-				 return ans;
-			 }
-			 map.put(nums[i], i);
-		 }	
-		return ans;		 
-	 }
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for(int i = 0; i < nums.length; i++) {
+			if(map.containsKey(target - nums[i])) {
+				return new int[]{map.get(target-nums[i]), i};
+			}else map.put(nums[i], i);
+		}
+		return new int[2];
+	}
 }
