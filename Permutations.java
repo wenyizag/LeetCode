dc7c71d7759fd90aa2b6bea9e2b3e5af12662386
Wenyi Zhang <wenyizag@outlook.com>
2018-03-01 15:06:41 -0500

    backtracking
    medium

--- Permutations.java
+++ Permutations.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 public class Permutations {
 	public static List<List<Integer>> permute(int[] nums) {
-		List<List<Integer>> ans = new ArrayList<List<Integer>>();
-		helper(nums, ans, new ArrayList<Integer>());
-        return ans;
-    }
-	
-	public static void helper(int[] nums, List<List<Integer>> ans, ArrayList<Integer> sol){
-		if(sol.size() == nums.length) {
-			ans.add(new ArrayList<Integer>(sol));
-		}else {
-			for(int i = 0; i < nums.length; i++) {
-				if(sol.contains(nums[i])) continue;
-				sol.add(nums[i]);
-				helper(nums, ans, sol);
-				sol.remove(sol.size() - 1);
-			}
-		}
-		
-	}
+		 ArrayList<List<Integer>> ans = new ArrayList<>();
+		 if(nums.length == 0) return ans;
+		 backtracking(nums, new ArrayList<Integer>(), ans);
+		 return ans;
+	 }
+
+	 public static void backtracking( int[] nums, ArrayList<Integer> cur, ArrayList<List<Integer>> ans) {
+		 if(cur.size() >= nums.length) {
+			 ans.add(new ArrayList<Integer>(cur));
+			 return;
+		 }
+		 for(int i = 0; i < nums.length; i++) {
+			 if(cur.contains(nums[i])) continue;
+			 cur.add(nums[i]);
+			 backtracking(nums, cur, ans);
+			 cur.remove(cur.size()-1);
+		 }
+	 }
 }
