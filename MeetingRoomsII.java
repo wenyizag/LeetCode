295c68da4ddac4c4de3baa455dc8598603eba683
Wenyi Zhang <wenyizag@outlook.com>
2018-03-06 17:48:47 -0500

    Sort
    Medium

--- MeetingRoomsII.java
+++ MeetingRoomsII.java
@@ -13,26 +13,26 @@ import leetcode2.Comparator;
 import leetcode2.Interval;
 
 public class MeetingRoomsII {
-	public int minMeetingRooms(Interval[] intervals) {
-        if(intervals.length == 0) return 0;
-        Arrays.sort(intervals, new Comparator<Interval>() {
-        		public int compare(Interval a, Interval b) {
-        			return a.start - b.start;
-        		}
-        });
-        PriorityQueue<Interval> heap = new PriorityQueue(intervals.length, new Comparator<Interval>(){
-        		public int compare(Interval a, Interval b) {return a.end - b.end;}
-        });
-        heap.offer(intervals[0]);
-        for(int i = 1; i < intervals.length; i++) {
-        		Interval curMin = heap.poll();
-        		if(curMin.end <= intervals[i].start) {
-        			curMin.end = intervals[i].end;
-        		}else {
-        			heap.add(intervals[i]);
-        		}
-        		heap.offer(curMin);
-        }
-        return heap.size();       
-    }
+	 public int minMeetingRooms(Interval[] intervals) {
+	        if(intervals.length == 0) return 0;
+	        Arrays.sort(intervals, new Comparotor<Interval>() {
+	        		public int compare(Interval a, Interval b) {
+	        			return a.start - b.start;
+	        		}
+	        });
+	        PriorityQueue<Interval> heap = new PriorityQueue(intervals.length, new Comparotor<Interval>(){
+	        		public int compare(Interval a, Interval b) {return a.end - b.end;}
+	        });
+	        heap.offer(intervals[0]);
+	        for(int i = 1; i < intervals.length; i++) {
+	        		Interval curMin = heap.poll();
+	        		if(curMin.end <= intervals[i].start) {
+	        			curMin.end = intervals[i].end;
+	        		}else {
+	        			heap.add(Intervals[i]);
+	        		}
+	        		heap.offer(curMin);
+	        }
+	        return heap.size();       
+	    }
 }
