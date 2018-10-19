536d52e5dd03b837b8980a6dc80d0f616a8e5dbd
Wenyi Zhang <wenyizag@outlook.com>
2018-10-19 18:43:50 -0400

    array

---
+++ 748ShortestCompletingWord.java
@@ -0,0 +1,10 @@
+class Solution extends SolBase {
+    public int rand10() {
+        int x = 0;
+        do{
+            x = (rand7() - 1) * 7 + rand7() - 1; 
+        }while(x >= 40);
+        
+        return x % 10 + 1;
+    }
+}
