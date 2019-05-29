ba3ac8dffb073377329f09912c1758e3208cc937
wenyizag <wenyizag@outlook.com>
2019-05-29 01:20:17 -0400

    Update LinkedListCycleII.java

--- LinkedListCycleII.java
+++ LinkedListCycleII.java
@@ -1,23 +1,21 @@
 package leetcode;
 
 public class LinkedListCycleII {
-	public static ListNode detectCycle(ListNode head) {
-		 ListNode fast = head;
-		 ListNode slow = head;
-		 //detect
-		 while(fast!= null && fast.next!= null) {
-			 fast = fast.next.next;
-			 slow = slow.next;
-			 if(slow == fast) {
-				 //cycle
-				 slow = head;
-				 while(fast != slow) {
-					 fast = fast.next;
-					 slow = slow.next;
-				 }
-				 return slow;
-			 }
-		 }		 
-		 return null;	        
-	 }
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast) break;
+        }
+
+        if(fast == null || fast.next == null) return null;
+        
+        slow = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
 }
