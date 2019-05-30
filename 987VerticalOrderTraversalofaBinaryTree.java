c10069cc89be02416ca034232fb7f53600f64b5c
wenyizag <wenyizag@outlook.com>
2019-05-30 15:48:58 -0400

    Update 987VerticalOrderTraversalofaBinaryTree.java

--- 987VerticalOrderTraversalofaBinaryTree.java
+++ 987VerticalOrderTraversalofaBinaryTree.java
@@ -1,48 +1,56 @@
-class Node {
-    TreeNode n;
-    int x;
-    int y;
-    Node(TreeNode n, int x, int y) {
-        this.n = n;
-        this.x = x;
-        this.y = y;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Node{
+    int val;
+    int level;
+    public Node(int val, int level) {
+        this.val = val;
+        this.level = level;
     }
 }
+
 class Solution {
-    int maxLevel = 0, minLevel = 0;
+
+    int min = 0, max = 0;
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if(root == null) return ans;
         
-        HashMap<Integer, TreeSet<Node>> map = new HashMap<>();
-        preorderTraverse(map, new Node(root, 0, 0));
-            
-        for(int i = minLevel, j = 0; i <= maxLevel; i++, j++) {
-            ans.add(new ArrayList<Integer>());
-            for(Node cur : map.get(i)) {
-                ans.get(j).add(cur.n.val);
-            }
-        }
+        HashMap<Integer, PriorityQueue<Node>> map = new HashMap<>();
+        DFS(root, map, 0, 0);
         
+        for(int i = min; i <= max; i++) {
+            List<Integer> list = new ArrayList<>();
+            PriorityQueue<Node> cur = map.get(i);
+            while(!cur.isEmpty()) {
+                list.add(cur.poll().val);
+            }
+            ans.add(list);
+        }
         return ans;
     }
     
-    public void preorderTraverse(HashMap<Integer, TreeSet<Node>> map, Node cur) {
-        // if(node == null) return;
-        minLevel = Math.min(cur.x, minLevel);
-        maxLevel = Math.max(cur.x, maxLevel);
-        if(!map.containsKey(cur.x)) {
-            map.put(cur.x, new TreeSet<Node>(new Comparator<Node>(){
+    public void DFS(TreeNode node, HashMap<Integer, PriorityQueue<Node>> map, int index, int level) {
+        if(node == null) return;
+        if(!map.containsKey(index)) {
+            min = Math.min(min, index);
+            max = Math.max(max, index);
+            map.put(index, new PriorityQueue<Node>(new Comparator<Node>(){
                 public int compare(Node a, Node b) {
-                    return (a.y != b.y) ? (a.y - b.y) : (a.n.val - b.n.val);
+                    return (a.level != b.level) ? a.level - b.level : a.val - b.val;
                 }
-            }));
+            })
+           );
         }
-        map.get(cur.x).add(cur);
-        if(cur.n.left != null) preorderTraverse(map, 
-                                new Node(cur.n.left, cur.x-1, cur.y + 1));
-        if(cur.n.right != null) preorderTraverse(map, 
-                                new Node(cur.n.right, cur.x+1, cur.y + 1));
-        
+        map.get(index).add(new Node(node.val, level));
+        if(node.left != null) DFS(node.left, map, index-1, level+1);
+        if(node.right != null) DFS(node.right, map, index+1, level+1);
     }
 }
