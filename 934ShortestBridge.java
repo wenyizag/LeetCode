cc745c9eb1e04601a17a8955f11b3bcc8172d1fd
wenyizag <wenyizag@outlook.com>
2019-05-30 23:47:51 -0400

    Update 934ShortestBridge.java

--- 934ShortestBridge.java
+++ 934ShortestBridge.java
@@ -1,73 +1,56 @@
-class Direction {
-    int x;
-    int y;
-    Direction(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
+
 
 class Solution {
     public int shortestBridge(int[][] A) {
         int row = A.length;
         int col = A[0].length;
         boolean[][] visited = new boolean[row][col];
-        //这个found变量是为了在发现第一座岛后停止DFS第二座岛
-        boolean found = false;
-        Direction[] dirs = {new Direction(0,1), 
-                            new Direction(0,-1), 
-                            new Direction(1,0), 
-                            new Direction(-1,0)};
-        
-        Queue<Direction> q = new LinkedList<>();
-        
-        //DFS to find the first island
-        for(int i = 0; i < row; i++){
-            if(found) break;
-            for(int j = 0; j < col; j++){
-                if(A[i][j] == 1){
-                    DFS(A, visited, q, dirs, i, j);
-                    found = true;
-                    break;
-                }   
-                // break;
-            }
-        }
-        
-        int step = 0;
-        //BFS to connect islands
-        System.out.println(q.size());
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                Direction cur = q.poll();
-                for(Direction d: dirs){
-                    int curRow = cur.x + d.x;
-                    int curCol = cur.y + d.y;
-                    if(curRow < 0 || curCol < 0 || curRow >= row || curCol >= col || visited[curRow][curCol]) continue;
-                    visited[curRow][curCol] = true;
-                    if(A[curRow][curCol] == 1) return step;
-                    q.offer(new Direction(curRow, curCol));
+        Queue<int[]> q = new LinkedList<>();
+        int[] dirs = {0, 1, 0, -1, 0};
+        //find the first island
+        outerloop:
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(A[i][j] == 1) {
+                    DFS(A, visited, q, row, col, i, j, dirs);
+                    break outerloop;
                 }
-                
             }
-            step++;
         }
-        return step;
+
+        int steps = 0;
+        // System.out.println(q.size());
+        //find the second island
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                int[] cur = q.poll();
+                for(int i = 0; i < 4; i++) {
+                    int nextX = cur[0] + dirs[i];
+                    int nextY = cur[1] + dirs[i+1];
+                    if(nextX<0 || nextY<0 || nextX>=row || nextY>=col || visited[nextX][nextY]) continue;
+                    visited[nextX][nextY] = true;
+                    if(A[nextX][nextY] == 1) return steps;
+                    else {
+                        q.add(new int[]{nextX, nextY});
+                    }
+                }
+            
+            }
+            steps++;
+        }
+        return -1;
     }
     
-    //DFS function
-    public void DFS(int[][] A, boolean[][] visited, Queue<Direction> q, Direction[] dirs, int i, int j){
-        int row = A.length;
-        int col = A[0].length;
-        
-        if(i < 0 || j < 0 || i >= row || j >= col || visited[i][j] || A[i][j] == 0) return;
-       
-        visited[i][j] = true;
-        q.offer(new Direction(i,j));
-        for(Direction cur: dirs){
-            DFS(A, visited, q, dirs, i+cur.x, j+cur.y);
+    //use DFS find the first island, and put its points into a queue
+    public void DFS(int[][] A, boolean[][] visited, Queue<int[]> q, int row, int col, int x, int y, int[] dirs){
+        //check for valid
+        if(x<0 || y<0 || x>=row || y >=col || visited[x][y] || A[x][y] == 0) return;
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+        for(int i = 0; i < 4; i++) {
+            DFS(A, visited, q, row, col, x+dirs[i], y+dirs[i+1], dirs);
         }
-            
     }
+    
 }
