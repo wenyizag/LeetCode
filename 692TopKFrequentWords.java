81e7084d2fdb098635b6056609948191814a849f
wenyizag <wenyizag@outlook.com>
2019-06-02 23:03:33 -0400

    Update 692TopKFrequentWords.java

--- 692TopKFrequentWords.java
+++ 692TopKFrequentWords.java
@@ -1,29 +1,26 @@
     public List<String> topKFrequent(String[] words, int k) {
 
-        //用hashmap统计出现次数
+        //using hashMap to store frequence
         HashMap<String, Integer> map = new HashMap<>();
         for(String cur : words){
-            map.put(cur, map.getOrDefault(cur, 0)+1);
-        }
-
-        //这里的compare没有用一个新的class写是因为需要用到map里的数据
-        PriorityQueue<String> q = new PriorityQueue( new Comparator<String>(){
-            public int compare(String s1, String s2){
+            map.put(cur, map.getOrDefault(cur, 0) + 1);
+        }        
+        
+        //using priorityQueue to store answer
+        PriorityQueue<String> q = new PriorityQueue<>(k+1, new Comparator<String>(){
+             public int compare(String s1, String s2){
                 if(map.get(s1) == map.get(s2)){
                     return -s1.compareTo(s2);
-                }else{
+                }else
                     return map.get(s1) - map.get(s2);
-                }
             }
         });
-        //用priorityQueu找出前k个
+        
         for(String cur : map.keySet()){
-            q.offer(cur);
-            if(q.size() > k){
-                q.poll();
-            }
+            q.add(cur);
+            if(q.size() > k) q.poll();
         }
-        //把priorityQueue里的转换到list，注意需倒着插，因为queue里是从小到大排列
+
         List<String> ans = new ArrayList<>();
         while(!q.isEmpty()){
             ans.add(0, q.poll());
