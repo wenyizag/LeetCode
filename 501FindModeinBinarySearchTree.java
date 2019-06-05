73658ee53e51d584976341812550cd53c065a42c
wenyizag <wenyizag@outlook.com>
2019-06-05 01:44:40 -0400

    Update 501FindModeinBinarySearchTree.java

--- 501FindModeinBinarySearchTree.java
+++ 501FindModeinBinarySearchTree.java
@@ -1,37 +1,35 @@
     
     //思路是利用BST的性质，如果我们按照postorder遍历，那么会得到一个递减的序列，所以当pre的值和cur的值不等的时候，就说明该值不会再出现了
     //那么把它的出现次数和mode比较
-    int mode = 0, cur = 0;
-    Stack<Integer> s = new Stack<>();
-    int pre = Integer.MIN_VALUE;
-    
+        List<Integer> list;
+    TreeNode pre = null;
+    int curCount = 0;
+    int maxCount = 0;
     public int[] findMode(TreeNode root) {
-        helper(root);
-        int[] ans = new int[s.size()];
-        int i = 0;
-        //将stack转换为array
-        while(!s.isEmpty()){
-            ans[i++] = s.pop();
-        }
+        list = new ArrayList<>();
+        
+        inOrder(root);
+        
+        int[] ans = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) ans[i] = list.get(i);
         return ans;
     }
-    public void helper(TreeNode node){
+    public void inOrder(TreeNode node) {
         if(node == null) return;
-        helper(node.right);
-        
-        if(node.val == pre || pre == Integer.MIN_VALUE){
-            cur += 1;
-        }else{
-            cur = 1;
+        inOrder(node.left);
+        if(pre == null || pre.val != node.val) {
+            curCount = 1;
+        }else {
+            curCount += 1;
         }
-        if(cur > mode){
-            s.clear();
-            s.push(node.val);
-            mode = cur;
-        }else if(cur == mode){
-            s.push(node.val);
+        if(curCount > maxCount) {
+            list.clear();
+            list.add(node.val);
+            maxCount = curCount;
+        }else if(curCount == maxCount) {
+            list.add(node.val);
         }
-        
-        pre = node.val;
-        helper(node.left);
+
+        pre = node;
+        inOrder(node.right);
     }
