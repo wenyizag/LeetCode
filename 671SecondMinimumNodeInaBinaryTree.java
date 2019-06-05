f8d2e0b418888cfba00dd8e881f353eb023413f9
wenyizag <wenyizag@outlook.com>
2019-06-04 21:32:42 -0400

    Update 671SecondMinimumNodeInaBinaryTree.java

--- 671SecondMinimumNodeInaBinaryTree.java
+++ 671SecondMinimumNodeInaBinaryTree.java
@@ -1,8 +1,8 @@
-    //我觉得我这个思路只用一遍遍历，比大众解法要好，美滋滋
-    int min = Integer.MAX_VALUE, secondMin = Integer.MAX_VALUE;
+ //我觉得我这个思路只用一遍遍历，比大众解法要好，美滋滋
+    Integer min = null, secondMin = null;
     public int findSecondMinimumValue(TreeNode root) {
         helper(root);
-        return (secondMin == Integer.MAX_VALUE) ? -1 : secondMin;
+        return (secondMin == null) ? -1 : secondMin;
     }
     //是这样的，有两个数来记录，分别是min和secondMin，那求min不用说了，就只要node的值比min小就更新min
     //在更新min的时候，同时将secondMin的值换成旧的min，那它肯定就是第二小的
@@ -10,12 +10,14 @@
     //这个时候我再需要把（node.val > min && node.val < secondMin）这样比较一下不就可以了吗。美滋滋
     public void helper(TreeNode node){
         if(node == null) return;
-        if(node.val < min){
+        if(min == null || node.val < min){
             secondMin = min;
             min = node.val;
-        }else if(node.val > min && node.val < secondMin){
+        }else if(node.val > min && (secondMin == null || node.val < secondMin)){
             secondMin = node.val;
         }
+
+        
         helper(node.left);
         helper(node.right);
     }
