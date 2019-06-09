c97a9d6bf594c08f24cf46001940f7f1791318ca
wenyizag <wenyizag@outlook.com>
2019-06-08 20:30:25 -0400

    Update 209MinimumSizeSubarraySum.java

--- 209MinimumSizeSubarraySum.java
+++ 209MinimumSizeSubarraySum.java
@@ -1,21 +1,49 @@
-   public int minSubArrayLen(int s, int[] nums) {
-        int min = nums.length;
-        if(min == 0) return 0;
-        boolean sum = false;
+//solution 1 两指针    
+public int minSubArrayLen(int s, int[] nums) {
+        int len = nums.length;
+        if(len == 0) return 0;
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        if(sum[0] >= s) return 1;
         
-        //两指针法，需注意的是如果所有和加起来都不足s，那么需要返回零，所以我加了个boolean变量
-        int cur = 0;
-        int left = 0;
-        for(int i = 0; i < nums.length; i++){
-            cur += nums[i];
-            while(left < i && (cur-nums[left]) >= s){
-                cur -= nums[left];
-                left++;
+
+        int left = 0, ans = nums.length;
+        for(int i = 1; i < nums.length; i++){
+            sum[i] = sum[i-1] + nums[i];
+            if(sum[i] >= s){
+                while(left < i && sum[i]-sum[left] >= s) left++;
+                ans = Math.min(ans, i-left+1);
             }
-            if(cur >= s){
-                min = Math.min(min, i-left+1);
-                sum = true;
-            }   
         }
-        return (sum) ? min : 0;
+        return (sum[len-1] >= s) ? ans : 0;
+
+    }
+
+//solution 2
+    public int minSubArrayLen(int s, int[] nums) {
+        int len = nums.length;
+        if(len == 0) return 0;
+        // pre sum
+        int[] preSum = new int[len+1];
+        for(int i = 0; i < len; i++) {
+            preSum[i+1] = preSum[i] + nums[i];
+            if(nums[i] >= s) return 1;
+        }
+        
+        if(preSum[len] < s) return 0;
+        int ans = len;
+        for(int i = len-1; i >= 0 && preSum[i+1] >= s; i--){
+            int high = i, low = 0, mid, target = preSum[i+1]-s;
+            while(high > low){
+                mid = low + (high-low)/2;
+                // System.out.println("high "+high+" low "+low+" mid "+mid);
+                if(preSum[mid] <= target){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+            ans = Math.min(ans, i-low+2);
+        }
+        return ans;
     }
