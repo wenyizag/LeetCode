7a66febe0719e8c32c4d1368579b12b77b60d6da
wenyizag <wenyizag@outlook.com>
2019-06-09 19:47:50 -0400

    Update SubsetII.java

--- SubsetII.java
+++ SubsetII.java
@@ -9,24 +9,29 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 
-public class SubsetII {
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
 	
-	public static List<List<Integer>> subsetsWithDup(int[] nums) {
-        ArrayList<List<Integer>> ans = new ArrayList<>();
-        if(nums.length == 0) return ans;
-        backtracking(0, nums, new ArrayList<Integer>(), ans);
+        List<List<Integer>> ans = new ArrayList<>();
+        //两个要点，第一是要sort array, 不然像数组{4,4,4,1,4}，如果不sort{4，1，4}这样的subset就会被重复添加，
+        //nums[i] == nums[i+1]根本起不到作用
+        Arrays.sort(nums);
+        helper(nums, 0, ans, new ArrayList<>());
         return ans;
     }
-	
-	public static void backtracking(int start, int[] nums, ArrayList<Integer> cur, ArrayList<List<Integer>> ans){
-		ans.add(new ArrayList<Integer>(cur));
-		for(int i = start; i < nums.length; i++) {
-			cur.add(nums[i]);
-			backtracking(i+1, nums, cur, ans);
-			cur.remove(cur.size()-1);
-			while(i < nums.length - 1 && nums[i] == nums[i+1]) i++;
-		}
-	}
+    
+    public void helper(int[] nums, int start, 
+    		List<List<Integer>> ans, List<Integer> list) {
+    		ans.add(new ArrayList<Integer>(list));
+    		for(int i = start; i < nums.length; i++) {
+    			list.add(nums[i]);
+    			helper(nums, i+1, ans, list);
+    			list.remove(list.size()-1);
+                //第二个是在backtracking后进行除重
+                while(i<nums.length-1 && nums[i] == nums[i+1]) i++;  
+    		}
+    		return;
+    }
 	 
 	 
 }
