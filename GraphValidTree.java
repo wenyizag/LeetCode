f2b848ce047d36523c306364221a8d0ccce6979d
wenyizag <wenyizag@outlook.com>
2019-06-09 21:27:45 -0400

    Update GraphValidTree.java

--- GraphValidTree.java
+++ GraphValidTree.java
@@ -28,3 +28,53 @@ public class GraphValidTree {
 		return n;
 	}
 }
+
+//solution 2. Union and Find，刚刚自己写的一个比较完整的union and find
+public class Solution {
+    /**
+     * @param n: An integer
+     * @param edges: a list of undirected edges
+     * @return: true if it's a valid tree, or false
+     */
+    public boolean validTree(int n, int[][] edges) {
+        // write your code here
+        if(edges.length != n-1) return false;
+        UnionAndFind unionEdges = new UnionAndFind(n);
+        for(int[] edge : edges) {
+            if(!unionEdges.union(edge[0], edge[1])) return false;
+        }
+        return true;
+    }
+
+class UnionAndFind {
+    int[] parent;
+    int[] rank;
+    public UnionAndFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for(int i = 0; i<n; i++) 
+            parent[i] = i;
+    }
+    
+    public int find(int i) {
+        if(parent[i] != i) parent[i] = find(parent[i]);
+        return parent[i];
+    }
+    
+    public boolean union(int i, int j) {
+        int p_i = find(i);
+        int p_j = find(j);
+        if(p_i == p_j) return false;
+        //union
+        if(rank[p_i] == rank[p_j]) {
+            parent[p_j] = p_i;
+            rank[p_i]++;
+        }else if(rank[p_i] > rank[p_j]) {
+            parent[p_j] = p_i;
+        }else {
+            parent[p_i] = p_j;
+        }
+        return true;
+    }
+}
+}
